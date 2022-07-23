65c1703a1bd2b3d423469a0aeb67a4a22ea4d308
画船听雨眠 <92084058+sankexilianhua@users.noreply.github.com>
2022-07-23 17:37:37 +0800

    Add files via upload

---
+++ Demo01.java
@@ -0,0 +1,17 @@
+package operator;
+
+public class Demo01 {
+    public static void main(String[] args) {
+        //二元运算符
+        //Ctrl+D:复制当前行到下一行
+        int a=10;
+        int b=20;
+        int c=25;
+        int d=25;
+
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+    }
+}
